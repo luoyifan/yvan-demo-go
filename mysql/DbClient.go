c0da24fb5b8bdc06f95a05734776a610acaaddd7
luoyifan <luoyifan@qq.com>
2019-05-09 13:23:16 +0800

    dbClient.query

--- mysql/DbClient.go
+++ mysql/DbClient.go
@@ -2,17 +2,24 @@ package mysql
 
 import (
 	"database/sql"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/pingcap/errors"
+	"github.com/siddontang/go-log/log"
+	"github.com/siddontang/go/hack"
+	"strconv"
+	"time"
 )
 
 type DbClient struct {
 	db *sql.DB
 }
 
-func checkErr(err error) {
-	if err != nil {
-		panic(err)
-	}
+type DbReader struct {
+	Columns     []string
+	ColumnNames map[string]int
+	Rows        [][]interface{}
+	Cost        time.Duration
 }
 
 func OpenMySql(url string, maxOpenConn int, maxIdleConn int) (*DbClient, error) {
@@ -32,7 +39,11 @@ func OpenMySql(url string, maxOpenConn int, maxIdleConn int) (*DbClient, error) 
 	return &dbClient, nil
 }
 
-func (self DbClient) execute(query string, args ...interface{}) (sql.Result, error) {
+func (self *DbClient) Ping() error {
+	return self.db.Ping()
+}
+
+func (self *DbClient) Execute(query string, args ...interface{}) (sql.Result, error) {
 	stmt, err := self.db.Prepare(query)
 	if err != nil {
 		return nil, err
@@ -41,7 +52,11 @@ func (self DbClient) execute(query string, args ...interface{}) (sql.Result, err
 	return stmt.Exec(args...)
 }
 
-func (self DbClient) query(query string, args ...interface{}) (*[]map[string]interface{}, error) {
+func (self *DbClient) Query(query string, args ...interface{}) (*DbReader, error) {
+
+	log.Infof("query:%s, params:%v", query, args)
+
+	t1 := time.Now()
 	stmt, err := self.db.Prepare(query)
 	if err != nil {
 		return nil, err
@@ -51,6 +66,7 @@ func (self DbClient) query(query string, args ...interface{}) (*[]map[string]int
 	if err != nil {
 		return nil, err
 	}
+	cost := time.Since(t1)
 
 	defer stmt.Close()
 	defer rows.Close()
@@ -60,25 +76,240 @@ func (self DbClient) query(query string, args ...interface{}) (*[]map[string]int
 		return nil, err
 	}
 
+	length := len(columns)
+	columnNames := make(map[string]int, length)
+	for i, n := range columns {
+		columnNames[n] = i
+	}
+
 	scanArgs := make([]interface{}, len(columns))
 	values := make([]interface{}, len(columns))
 	for i := range values {
 		scanArgs[i] = &values[i]
 	}
-
-	list := make([]map[string]interface{}, len(columns))
+	var list [][]interface{}
 	for rows.Next() {
-		err = rows.Scan(scanArgs...)
-		if err != nil {
-			return nil, err
+		e := rows.Scan(scanArgs...)
+		if e != nil {
+			return nil, e
 		}
-		record := make(map[string]interface{})
-		for i, col := range values {
-			if col != nil {
-				record[columns[i]] = col
-			}
+		data := make([]interface{}, length)
+		for i, v := range scanArgs {
+			data[i] = *(v.(*interface{}))
 		}
-		list = append(list, record)
+		list = append(list, data)
 	}
-	return &list, nil
+
+	log.Infof("rows:%d, columns:%d, cost:%s", len(list), length, cost)
+
+	return &DbReader{
+		Columns:     columns,
+		Rows:        list,
+		ColumnNames: columnNames,
+		Cost:        cost,
+	}, nil
+}
+
+func (r *DbReader) ColumnIndex(name string) (int, error) {
+	if column, ok := r.ColumnNames[name]; ok {
+		return column, nil
+	} else {
+		return 0, errors.Errorf("invalid field name %s", name)
+	}
+}
+
+func (r *DbReader) RowLength() int {
+	return len(r.Rows)
+}
+
+func (r *DbReader) ColumnLength() int {
+	return len(r.Columns)
+}
+
+func (r *DbReader) GetValue(row int, column int) (interface{}, error) {
+	if row >= len(r.Rows) || row < 0 {
+		return nil, errors.Errorf("invalid row index %d", row)
+	}
+
+	if column >= len(r.Columns) || column < 0 {
+		return nil, errors.Errorf("invalid column index %d", column)
+	}
+
+	return r.Rows[row][column], nil
+}
+
+func (r *DbReader) GetValueByName(row int, name string) (interface{}, error) {
+	if column, err := r.ColumnIndex(name); err != nil {
+		return nil, errors.Trace(err)
+	} else {
+		return r.GetValue(row, column)
+	}
+}
+
+func (r *DbReader) IsNull(row, column int) (bool, error) {
+	d, err := r.GetValue(row, column)
+	if err != nil {
+		return false, err
+	}
+
+	return d == nil, nil
+}
+
+func (r *DbReader) IsNullByName(row int, name string) (bool, error) {
+	if column, err := r.ColumnIndex(name); err != nil {
+		return false, err
+	} else {
+		return r.IsNull(row, column)
+	}
+}
+
+func (r *DbReader) GetUint(row, column int) (uint64, error) {
+	d, err := r.GetValue(row, column)
+	if err != nil {
+		return 0, err
+	}
+
+	switch v := d.(type) {
+	case int:
+		return uint64(v), nil
+	case int8:
+		return uint64(v), nil
+	case int16:
+		return uint64(v), nil
+	case int32:
+		return uint64(v), nil
+	case int64:
+		return uint64(v), nil
+	case uint:
+		return uint64(v), nil
+	case uint8:
+		return uint64(v), nil
+	case uint16:
+		return uint64(v), nil
+	case uint32:
+		return uint64(v), nil
+	case uint64:
+		return uint64(v), nil
+	case float32:
+		return uint64(v), nil
+	case float64:
+		return uint64(v), nil
+	case string:
+		return strconv.ParseUint(v, 10, 64)
+	case []byte:
+		return strconv.ParseUint(string(v), 10, 64)
+	case nil:
+		return 0, nil
+	default:
+		return 0, errors.Errorf("data type is %T", v)
+	}
+}
+
+func (r *DbReader) GetUintByName(row int, name string) (uint64, error) {
+	if column, err := r.ColumnIndex(name); err != nil {
+		return 0, err
+	} else {
+		return r.GetUint(row, column)
+	}
+}
+
+func (r *DbReader) GetInt(row, column int) (int64, error) {
+	v, err := r.GetUint(row, column)
+	if err != nil {
+		return 0, err
+	}
+
+	return int64(v), nil
+}
+
+func (r *DbReader) GetIntByName(row int, name string) (int64, error) {
+	v, err := r.GetUintByName(row, name)
+	if err != nil {
+		return 0, err
+	}
+
+	return int64(v), nil
+}
+
+func (r *DbReader) GetFloat(row, column int) (float64, error) {
+	d, err := r.GetValue(row, column)
+	if err != nil {
+		return 0, err
+	}
+
+	switch v := d.(type) {
+	case int:
+		return float64(v), nil
+	case int8:
+		return float64(v), nil
+	case int16:
+		return float64(v), nil
+	case int32:
+		return float64(v), nil
+	case int64:
+		return float64(v), nil
+	case uint:
+		return float64(v), nil
+	case uint8:
+		return float64(v), nil
+	case uint16:
+		return float64(v), nil
+	case uint32:
+		return float64(v), nil
+	case uint64:
+		return float64(v), nil
+	case float32:
+		return float64(v), nil
+	case float64:
+		return v, nil
+	case string:
+		return strconv.ParseFloat(v, 64)
+	case []byte:
+		return strconv.ParseFloat(string(v), 64)
+	case nil:
+		return 0, nil
+	default:
+		return 0, errors.Errorf("data type is %T", v)
+	}
+}
+
+func (r *DbReader) GetFloatByName(row int, name string) (float64, error) {
+	if column, err := r.ColumnIndex(name); err != nil {
+		return 0, err
+	} else {
+		return r.GetFloat(row, column)
+	}
+}
+
+func (r *DbReader) GetString(row, column int) (string, error) {
+	d, err := r.GetValue(row, column)
+	if err != nil {
+		return "", err
+	}
+
+	switch v := d.(type) {
+	case string:
+		return v, nil
+	case []byte:
+		return hack.String(v), nil
+	case int, int8, int16, int32, int64,
+		uint, uint8, uint16, uint32, uint64:
+		return fmt.Sprintf("%d", v), nil
+	case float32:
+		return strconv.FormatFloat(float64(v), 'f', -1, 64), nil
+	case float64:
+		return strconv.FormatFloat(v, 'f', -1, 64), nil
+	case nil:
+		return "", nil
+	default:
+		return "", errors.Errorf("data type is %T", v)
+	}
+}
+
+func (r *DbReader) GetStringByName(row int, name string) (string, error) {
+	if column, err := r.ColumnIndex(name); err != nil {
+		return "", err
+	} else {
+		return r.GetString(row, column)
+	}
 }
--- mysql/DbClient_test.go
+++ mysql/DbClient_test.go
@@ -1,7 +1,7 @@
 package mysql
 
 import (
-	"fmt"
+	"github.com/siddontang/go-log/log"
 	"testing"
 )
 
@@ -9,22 +9,48 @@ const (
 	DB_Driver = "b2b_5:123456@tcp(10.3.5.30:3306)/b2b_third?charset=utf8"
 )
 
-func TestOpenMySql(t *testing.T) {
-	dbClient, err := OpenMySql(DB_Driver, 50, 1000)
-	if err != nil {
-		panic(err)
-	}
+var dbClient *DbClient
 
-	rows, err := dbClient.query(`SELECT
+func init() {
+	dbClient, _ = OpenMySql(DB_Driver, 50, 1000)
+}
+
+func TestOpenMySql(t *testing.T) {
+	t.Log(dbClient.Ping())
+}
+
+func Benchmark_DbClient_Query(b *testing.B) {
+	log.Infof("benchmark.query n=%d", b.N)
+	for i := 0; i < b.N; i++ { //use b.N for looping
+		dbClient.Query(`SELECT
 activity_id, 
 activity_name, 
 activity_content, 
 create_by,
 create_at
-FROM tb_activity where site_id =?`, "1111111112")
+FROM tb_activity where site_id=?`, "1111111112")
+	}
+}
+
+func TestDbClient_Query(t *testing.T) {
+	reader, err := dbClient.Query(`SELECT
+activity_id, 
+activity_name, 
+activity_content, 
+create_by,
+create_at
+FROM tb_activity where site_id=?`, "1111111112")
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%v\r\n", rows)
+	for i := 0; i < reader.RowLength(); i++ {
+
+		for _, c := range reader.Columns {
+			v, _ := reader.GetStringByName(i, c)
+			log.Infof("row[%d][%s]=%s", i, c, v)
+		}
+
+		log.Info("---")
+	}
 }
